a259ddaf9e5c8cbc7b7b1b3a55fca3830d6e30a5
Капралов Яков Владиславович <70843236+prorockk@users.noreply.github.com>
2020-10-03 22:48:15 +0400

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    if (n<0) {
+        n*=-1
+    }
+    let xui = Array.from(n.toString())
+    let ressult= xui.reverse().join('')
+    return ressult
 }
